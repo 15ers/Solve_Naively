e7171477b618ffd61651b12d85ae1c22b0fe7892
epikjjh <epikjjh@gmail.com>
2019-09-14 13:10:02 +0000

    Commit from python container baekjoon problem 17413 using regex
    Signed-off-by: epikjjh <epikjjh@gmail.com>

--- epikjjh/baekjoon/17413.py
+++ epikjjh/baekjoon/17413.py
@@ -1,31 +1,13 @@
-def gen(stream):
-    stack = []
-    tmp = ""
-    for e in stream:
-        if e == " ":
-            if stack:
-                tmp += e
-            else:
-                if tmp:
-                    yield tmp[::-1]
-                    yield e
-                    tmp = ""
-        else:
-            if e == "<":
-                yield tmp[::-1]
-                stack.append(e)
-                tmp = e
-            elif e == ">":
-                stack.pop(0)
-                tmp += e
-                yield tmp
-                tmp = ""
-            else:
-                tmp += e
-    if tmp:
-        yield tmp[::-1]
+import re
 
-                
 stream = input()
-ans = "".join(list(gen(stream)))
+p = re.compile("<?[^<>]+>?")
+ans = ""
+for elem in p.findall(stream):
+    if elem[0] == "<":
+        ans += elem
+    else:
+        for e in elem.split():
+            ans += e[::-1] + " "
+        ans = ans.rstrip()
 print(ans)
