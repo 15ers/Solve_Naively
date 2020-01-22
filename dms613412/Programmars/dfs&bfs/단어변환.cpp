0c336f1fd8e0f1b3f94655a8cf7cc25d9ee71c50
unknown <dms613412@naver.com>
2020-01-22 23:52:55 +0900

    dfs/bfs

---
+++ dms613412/Programmars/dfs&bfs/단어변환.cpp
@@ -0,0 +1,36 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int answer = 51;
+int size, wsize;
+int visited[50] = { 0, };
+
+void dfs(int num, string word, vector<string> &words, string &target) {
+	if (word == target) { answer = min(answer, num); return; }
+	if (num == wsize)return;
+
+	for (int i = 0; i < wsize; ++i) {
+		if (visited[i])continue;
+		int count = 0;
+		for (int j = 0; j < size; ++j) {
+			if (word[j] != words[i][j])++count;
+			if (count > 1)break;
+		}
+		if (count == 1) {
+			visited[i] = 1;
+			dfs(num + 1, words[i], words, target);
+			visited[i] = 0;
+		}
+	}
+
+}
+
+int solution(string begin, string target, vector<string> words) {
+	size = begin.size(); wsize = words.size();
+	dfs(0, begin, words, target);
+
+	if (answer == 51)return 0;
+	return answer;
+}
---
+++ dms613412/Programmars/dfs&bfs/여행경로.cpp
@@ -0,0 +1,98 @@
+﻿/*
+//내림차순 정렬
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <functional>  
+using namespace std;
+
+int visited[10000] = { 0, };
+vector<string> answer, tmp;
+int size;
+
+void dfs(int num, string word, vector<vector<string>> &tickets) {
+	tmp.push_back(word);
+
+	if (num == size) {
+		answer = tmp;
+		tmp.pop_back();
+		return;
+	}
+
+	for (int i = 0; i < size; ++i) {
+		if (visited[i])continue;
+		if (word == tickets[i][0]) {
+			visited[i] = 1;
+			dfs(num + 1, tickets[i][1], tickets);
+			visited[i] = 0;
+		}
+	}
+	tmp.pop_back();
+}
+
+bool comp(vector<string> a, vector<string> b) {
+	if (a[0].compare(b[0]) < 0) {
+		return false;
+	}
+	else if (a[0].compare(b[0]) > 0) {
+		return true;
+	}
+	else {
+		if (a[1].compare(b[1]) < 0) {
+			return false;
+		}
+		else if (a[1].compare(b[1]) > 0) {
+			return true;
+		}
+	}
+	return true;
+}
+
+vector<string> solution(vector<vector<string>> tickets) {
+	sort(tickets.begin(), tickets.end(), comp);
+	size = tickets.size();
+	dfs(0, "ICN", tickets);
+	return answer;
+}
+*/
+
+//오름차순 정렬 -> 첫 경로 찾으면 dfs문 종료
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <functional>  
+using namespace std;
+
+int visited[10000] = { 0, };
+vector<string> answer, tmp;
+int size;
+
+bool dfs(int num, string word, vector<vector<string>> &tickets) {
+	tmp.push_back(word);
+
+	if (num == size) {
+		answer = tmp;
+		tmp.pop_back();
+		return true;
+	}
+
+	for (int i = 0; i < size; ++i) {
+		if (visited[i])continue;
+		if (word == tickets[i][0]) {
+			visited[i] = 1;
+			if (dfs(num + 1, tickets[i][1], tickets))return true;
+			visited[i] = 0;
+		}
+	}
+	tmp.pop_back();
+	return false;
+}
+
+vector<string> solution(vector<vector<string>> tickets) {
+	sort(tickets.begin(), tickets.end());
+	size = tickets.size();
+	dfs(0, "ICN", tickets);
+	return answer;
+}
