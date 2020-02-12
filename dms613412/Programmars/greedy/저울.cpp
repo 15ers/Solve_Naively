2648505f79e428b4933d880961d4c101d903f952
unknown <dms613412@naver.com>
2020-02-13 00:43:31 +0900

    이분탐색 & greedy

---
+++ dms613412/Programmars/greedy/저울.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+동일 i 기준, 누적합[i]+1가 원배열[i+1] 이상이어야 하며, 미만이되면 그 때의 누적합[i]+1부터 만들 수 없는 수.
+모두 비교하고도 통과 되었다면, 마지막 수인 50+1=51이 만들 수 없는 수.(모든 수들의 합+1)
+*/
+
+int solution(vector<int> weight) {
+	sort(weight.begin(), weight.end());
+	int size = weight.size();
+	vector<int> vec;
+	vec.push_back(weight[0]);
+	for (int i = 1; i < size; ++i) {
+		if (vec[i - 1] + 1 < weight[i])return vec[i - 1] + 1;
+		vec.push_back(weight[i] + vec[i - 1]);
+	}
+
+	return vec[size - 1] + 1;
+}
---
+++ dms613412/Programmars/이분탐색/예산.cpp
@@ -0,0 +1,36 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> budgets, int M) {
+	sort(budgets.begin(), budgets.end());
+	int size = budgets.size();
+	int left = 0, right = size - 1, mid = (left + right) / 2;
+	long long sum;
+
+	while (left <= right) {
+		sum = 0;
+		for (int i = 0; i < mid; ++i)
+			sum += budgets[i];
+
+		if (sum + budgets[mid] * (size - mid) < M) {
+			if (mid == size - 1)return budgets[mid];
+			left = mid + 1; mid = (left + right) / 2;
+		}
+		else if (sum + budgets[mid] * (size - mid) == M) {
+			return budgets[mid];
+		}
+		else {
+			if (mid == 0)return M / size;
+			long long tmp = (M - sum) / (size - mid);
+			if (tmp >= budgets[mid - 1]) return tmp;
+			right = mid - 1; mid = (left + right) / 2;
+		}
+	}
+
+	int answer = 0;
+	return answer;
+}
