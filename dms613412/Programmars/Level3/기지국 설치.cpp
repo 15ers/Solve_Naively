2f1bf3887e85770df13664988fd516e1173e6dd2
unknown <dms613412@naver.com>
2020-02-24 16:54:04 +0900

    level3

---
+++ dms613412/Programmars/Level3/기지국 설치.cpp
@@ -0,0 +1,62 @@
+﻿/*
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, vector<int> stations, int w)
+{
+	int answer = 0;
+	vector<int> vec(n,0);
+
+	for(auto &x:stations){
+		fill(((x-1-w<0)?0:x-1-w)+vec.begin(), ((x-1+w>=n)?n-1:x-1+w)+vec.begin()+1, 1);
+	}
+
+
+	int start=0,end=0;
+	while(end<n && start<n){
+		start=find(vec.begin()+end, vec.end(),0)-vec.begin();
+		end=find(vec.begin()+start+1, vec.end(),1)-vec.begin();
+		//cout<<start<<" "<<end<<endl;
+		answer += (end-start)/(2*w+1);
+		if((end-start)%(2*w+1)>0) ++answer;
+	}
+
+	return answer;
+}*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, vector<int> stations, int w)
+{
+	int answer = 0;
+
+	int start = 0, end = 0;
+	for (auto &x : stations) {
+		end = x - 1 - w;
+		if (end <= start) {
+			start = x + w;
+			continue;
+		}
+		answer += (end - start) / (2 * w + 1);
+		if ((end - start) % (2 * w + 1) > 0) ++answer;
+		start = x + w;
+
+	}
+
+	if (start < n) {
+		end = n;
+		answer += (end - start) / (2 * w + 1);
+		if ((end - start) % (2 * w + 1) > 0) ++answer;
+	}
+
+
+
+	return answer;
+}
---
+++ dms613412/Programmars/Level3/보행자 천국.cpp
@@ -0,0 +1,42 @@
+﻿#include <vector>
+
+using namespace std;
+
+int MOD = 20170805;
+
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+int solution(int m, int n, vector<vector<int>> city_map) {
+	if (city_map[0][0] == 1)return 0;
+	vector<vector<pair<int, int>>> vec(m, vector<pair<int, int>>(n, { 0,0 }));
+	vec[0][0] = { 1,1 };
+	for (int i = 1; i < m; ++i) {
+		if (city_map[i][0] == 1)break;
+		vec[i][0].second = 1;
+	}
+	for (int i = 1; i < n; ++i) {
+		if (city_map[0][i] == 1)break;
+		vec[0][i].first = 1;
+	}
+
+	for (int i = 1; i < m; ++i) {
+		for (int j = 1; j < n; ++j) {
+			if (city_map[i][j] == 1)continue;
+			if (city_map[i - 1][j] == 0) {
+				vec[i][j].second = (vec[i - 1][j].first + vec[i - 1][j].second) % 20170805;
+			}
+			else if (city_map[i - 1][j] == 2) {
+				vec[i][j].second = vec[i - 1][j].second % 20170805;
+			}
+
+			if (city_map[i][j - 1] == 0) {
+				vec[i][j].first = (vec[i][j - 1].first + vec[i][j - 1].second) % 20170805;
+			}
+			else if (city_map[i][j - 1] == 2) {
+				vec[i][j].first = vec[i][j - 1].first % 20170805;
+			}
+		}
+	}
+
+
+	return (vec[m - 1][n - 1].first + vec[m - 1][n - 1].second) % 20170805;
+}
