a68feeac3834343ed4bb304d22d939e7df3564bb
Jo Hyung Jung <epikjjh@gmail.com>
2020-04-12 23:46:27 +0900

    Commit from C/C++ container baekjoon problem: 1766 2252 2623
    Signed-off-by: Jo Hyung Jung <epikjjh@gmail.com>

--- epikjjh/baekjoon/2252.cpp
+++ epikjjh/baekjoon/2252.cpp
@@ -3,26 +3,24 @@
 using namespace std;
 
 int main(){
-	int n,m;
-	cin >> n >> m;
-	vector<vector<int>> adj(n+1);
-	vector<int> indegree(n+1),ret;
-	for(int i=0;i<m;i++){
-		int u,v;
-		cin >> u >> v;
-		adj[u].push_back(v);
-		indegree[v]++;
-	}
-	queue<int> q;
-	for(int i=1;i<=n;i++)	if(!indegree[i])	q.push(i);
-	for(int i=0;i<n;i++){
-		int cur = q.front();
-		q.pop();
-		for(int nxt: adj[cur])	if(!(--indegree[nxt]))	q.push(nxt);
-		ret.push_back(cur);
-	}
-	for(int &p: ret)	cout << p << " ";
-	cout << endl;
-	
-	return 0;
+    int n,m;
+    cin >> n >> m;
+    vector<int> indegree(n+1);
+    vector<vector<int>> adj(n+1);
+    while(m--){
+        int a,b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        indegree[b]++;
+    }
+    queue<int> q;
+    for(int i=1;i<=n;i++)   if(!indegree[i])    q.push(i);
+    while(!q.empty()){
+        int cur=q.front();
+        q.pop();
+        cout << cur << " ";
+        for(int nxt: adj[cur])  if(!(--indegree[nxt]))  q.push(nxt);
+    }
+
+    return 0;
 }
--- epikjjh/baekjoon/2623.cpp
+++ epikjjh/baekjoon/2623.cpp
@@ -3,37 +3,32 @@
 using namespace std;
 
 int main(){
-	int n,m;
-	cin >> n >> m;
-	vector<vector<int>> adj(n+1);
-	vector<int> indegree(n+1);
-	for(int i=0;i<m;i++){
-		int k,pre,cur;
-		cin >> k;
-		if(!k)	continue;
-		cin >> pre;
-		for(int j=1;j<k;j++){
-			cin >> cur;
-			adj[pre].push_back(cur);
-			indegree[cur]++;
-			pre = cur;
-		}
-	}
-	queue<int> q;
-	vector<int> ret;
-	for(int i=1;i<=n;i++)	if(!indegree[i])	q.push(i);
-	for(int i=0;i<n;i++){
-		if(q.empty()){
-			cout << 0 << endl;
-			return 0;
-		}
-		int cur = q.front();
-		q.pop();
-		ret.push_back(cur);
-		for(int nxt: adj[cur])
-			if(!(--indegree[nxt]))	q.push(nxt);
-	}
-	for(int &ans: ret)	cout << ans << endl;
-	
-	return 0;
+    int n,m;
+    cin >> n >> m;
+    vector<int> indegree(n+1),ret;
+    vector<vector<int>> adj(n+1);
+    while(m--){
+        int num,pre=-1,cur=-1;
+        cin >> num;
+        while(num--){
+            cin >> cur;
+            if(pre!=-1){
+                adj[pre].push_back(cur);
+                indegree[cur]++;
+            }
+            pre = cur;
+        }
+    }
+    queue<int> q;
+    for(int i=1;i<=n;i++)   if(!indegree[i])    q.push(i);
+    while(!q.empty()){
+        int cur=q.front();
+        q.pop();
+        ret.push_back(cur);
+        for(int nxt: adj[cur])  if(!(--indegree[nxt]))  q.push(nxt);
+    }
+    if(ret.size()!=n)   cout << 0 << endl;
+    else    for(int p: ret) cout << p << endl;
+
+    return 0;
 }
